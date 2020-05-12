a249c2ff6c4daca6e39a86c5e9cbd00913cb1395
Philip Hamner <philiphamner@gmail.com>
2020-05-12 16:31:32 -0700

    Add index.html and index.jsx

---
+++ index.jsx
@@ -0,0 +1,13 @@
+
+// inside your index.jsx
+import React,
+{ Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    <div>Something Awesome</div>
+  }
+}
+window.Reviews = App;
